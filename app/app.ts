a1b63b3496f7e509083b60856a054ed8a964c669
c-alexmala <c-alexandru.malai@edifecs.com>
2018-03-21 19:37:46 +0200

    added search box component

--- app/app.ts
+++ app/app.ts
@@ -2,17 +2,18 @@ import {Component} from "@angular/core";
 import {NgModule} from "@angular/core";
 import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 import {BrowserModule} from "@angular/platform-browser";
+import {SearchBox} from "./search-box.component";
 
 @Component({
     selector: 'app',
-    template: `Hello Nh2!`
+    template: `<search-box [placeholder]="'Custom Placeholder'"></search-box>`
 })
 export class App {
 
 }
 
 @NgModule({
-    declarations: [App],
+    declarations: [App, SearchBox],
     imports: [BrowserModule],
     bootstrap: [App]
 })
